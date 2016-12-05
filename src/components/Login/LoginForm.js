e38d2e9cec7a4bd9e94f059d92f179742ffa130e
Dido Viktorov <d.viktorovchak@gmail.com>
2016-12-05 22:22:47 +0200

    correcting Login form view + adding Login.css

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,32 +1,37 @@
 import React, {Component} from 'react';
+import './Login.css';
 
 export default class LoginForm extends Component {
     render() {
         return (
             <form onSubmit={this.props.onSubmit}>
-                <div className="form-group">
-                    <label className="form-control">
-                        Username
-                    </label>
-                    <input
-                        className="form-control"
-                        type="text"
-                        name="username"
-                        required=""
-                        value={this.props.username}
-                        onChange={this.props.onChange}
-                    />
+                <div className="div-form">
+                    <div className="form-group">
+                        <label>
+                            <h3>Username</h3>
+                        </label>
+                        <input
+                            className="form-control"
+                            type="text"
+                            name="username"
+                            required=""
+                            value={this.props.username}
+                            onChange={this.props.onChange}
+                        />
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label className="form-control">
-                        Password
-                    </label>
-                    <input
-                        className="form-control"
-                        type="password" name="password"
-                        value={this.props.password}
-                        onChange={this.props.onChange}
-                    />
+                <div className="div-form">
+                    <div className="form-group">
+                        <label>
+                            <h3>Password</h3>
+                        </label>
+                        <input
+                            className="form-control"
+                            type="password" name="password"
+                            value={this.props.password}
+                            onChange={this.props.onChange}
+                        />
+                    </div>
                 </div>
                <input type="submit" value="Login" className="btn btn-default"/>
             </form>
