b233636419b4da35aa7cd590d5d7efc9e7d306f4
Dido Viktorov <d.viktorovchak@gmail.com>
2016-12-05 22:28:30 +0200

    correcting register view and adding register.css file

--- src/components/Register/RegisterForm.js
+++ src/components/Register/RegisterForm.js
@@ -1,45 +1,52 @@
 import React, {Component} from 'react';
+import './Register.css';
 
 export default class RegisterForm extends Component {
     render() {
         return (
             <form onSubmit={this.props.onSubmit}>
-                <div className="form-group">
-                    <label className="form-control">
-                        Username
-                    </label>
-                    <input
-                        className="form-control"
-                        type="text"
-                        name="username"
-                        value={this.props.username}
-                        onChange={this.props.onChange}
-                        required
-                    />
+                <div className="div-form">
+                    <div className="form-group">
+                        <label>
+                            <h3>Username</h3>
+                        </label>
+                        <input
+                            className="form-control"
+                            type="text"
+                            name="username"
+                            value={this.props.username}
+                            onChange={this.props.onChange}
+                            required
+                        />
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label className="form-control">
-                        Password
-                    </label>
-                    <input
-                        className="form-control"
-                        type="password" name="password"
-                        value={this.props.password}
-                        onChange={this.props.onChange}
-                        required
-                    />
+                <div className="div-form">
+                    <div className="form-group">
+                        <label>
+                            <h3>Password</h3>
+                        </label>
+                        <input
+                            className="form-control"
+                            type="password" name="password"
+                            value={this.props.password}
+                            onChange={this.props.onChange}
+                            required
+                        />
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label className="form-control">
-                        Confirm password
-                    </label>
-                    <input
-                        className="form-control"
-                        type="password" name="repeat"
-                        value={this.props.repeat}
-                        onChange={this.props.onChange}
-                        required
-                    />
+                <div className="div-form">
+                    <div className="form-group">
+                        <label>
+                            <h3>Confirm password</h3>
+                        </label>
+                        <input
+                            className="form-control"
+                            type="password" name="repeat"
+                            value={this.props.repeat}
+                            onChange={this.props.onChange}
+                            required
+                        />
+                    </div>
                 </div>
                 <input type="submit" value="Register" className="btn btn-default"/>
             </form>
