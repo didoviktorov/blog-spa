2f9d5ab1558882e66d0b393242344f0904c7a2f2
karabinata <karabinata1992@gmail.com>
2016-12-06 16:32:39 +0200

    Table content

--- src/components/ListPosts/ListPostsForm.js
+++ src/components/ListPosts/ListPostsForm.js
@@ -4,17 +4,18 @@ import {Link} from 'react-router';
 export default class Post extends Component {
 
     render() {
-        //let subContent = '';
-        //if(this.state.description.length > 25) {
-        //    subContent = this.props.description.substr(0, 25) + '...';
-        //} else {
-        //    subContent = this.props.description;
-        //}
+        let subContent = this.props.description;
+
+        if(subContent.length > 30) {
+            subContent = subContent.substr(0, 25) + '...';
+        } else {
+            subContent = this.props.description;
+        }
 
         return (
             <tr>
                 <td>{this.props.name}</td>
-                <td>{this.props.description}</td>
+                <td>{subContent}</td>
                 <td><Link to={"/post/" + this.props.id}>View more</Link></td>
             </tr>
         )
