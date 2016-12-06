768a65a54e8c3c7d3cb5ea5d0286f8b77b3d3abd
Dido Viktorov <d.viktorovchak@gmail.com>
2016-12-06 20:22:09 +0200

    Edit link

---
+++ src/components/VeiwPost/PostControls.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+
+export default class PostControls extends Component {
+    render() {
+        let edit = null;
+
+        if (this.props.canEdit) {
+            edit = <Link to={"/edit/" + this.props.postId} className="btn btn-default">Edit info</Link>;
+        }
+
+
+
+        return (
+            <div>
+                {edit}
+            </div>
+        )
+    }
+}
